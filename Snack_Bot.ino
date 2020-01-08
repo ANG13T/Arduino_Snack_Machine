77b4d9bff4e383887318161deb0666f932be4aca
angelina-tsuboi <angelina.t1832@gmail.com>
2020-01-07 21:11:45 -0800

    Sensor Functionality

--- Snack_Bot.ino
+++ Snack_Bot.ino
@@ -6,15 +6,48 @@ Servo leftServo;
 Servo centerServo;
 Servo rightServo;
 
+const int leftSensorTrig = 13;
+const int leftSensorEcho = 12;
+const int rightSensorTrig = 0;
+const int rightSensorEcho = 0;
+
+long duration;
+int distance;
+
 void setup() {
   // put your setup code here, to run once:
   leftServo.attach(6);
   rightServo.attach(7);
+  pinMode(leftSensorTrig, OUTPUT); 
+  pinMode(leftSensorEcho, INPUT);
+//  pinMode(rightSensorTrig, OUTPUT); 
+//  pinMode(rightSensorEcho, INPUT);
 }
 
 void loop() {
-   //Right Servo Code: 
-    servoMovement(100, 190, rightServo); 
+   //Right Servo Code:
+     distance = calculateDistance();
+     
+  if(distance < 20){
+    servoMovement(85, 160, leftServo); 
+    delay(1000);
+  }
+
+//servoMovement(70, 200, rightServo);     
+}
+
+//Sensor
+int calculateDistance(){ 
+  
+  digitalWrite(leftSensorTrig, LOW); 
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(leftSensorTrig, HIGH); 
+  delayMicroseconds(10);
+  digitalWrite(leftSensorTrig, LOW);
+  duration = pulseIn(leftSensorEcho, HIGH); // Reads the echoPin, returns the sound wave travel time in microseconds
+  distance= duration*0.034/2;
+  return distance;
 }
 
 void servoMovement(int start, int end, Servo servo){
@@ -25,7 +58,7 @@ void servoMovement(int start, int end, Servo servo){
       delay(15);                       
       } 
   
-  delay(1000);
+  delay(100);
   
     for(angle = end; angle>start; angle-=5)     // command to move from 180 degrees to 0 degrees 
     {                                
@@ -33,5 +66,5 @@ void servoMovement(int start, int end, Servo servo){
       servo.write(angle);   //command to rotate the servo to the specified angle
       delay(5);                       
     } 
-  delay(1000);
+  delay(100);
   }
