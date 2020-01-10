4c294f2a1fb69bec3c1a82426e1156059fee2444
angelina-tsuboi <angelina.t1832@gmail.com>
2020-01-09 20:54:54 -0800

    Right and Left Sensors both detect and run servo motors

--- Snack_Bot.ino
+++ Snack_Bot.ino
@@ -8,11 +8,11 @@ Servo rightServo;
 
 const int leftSensorTrig = 13;
 const int leftSensorEcho = 12;
-const int rightSensorTrig = 0;
-const int rightSensorEcho = 0;
+const int rightSensorTrig = 11;
+const int rightSensorEcho = 10;
 
-long duration;
-int distance;
+int leftDistance;
+int rightDistance;
 
 void setup() {
   // put your setup code here, to run once:
@@ -20,25 +20,29 @@ void setup() {
   rightServo.attach(7);
   pinMode(leftSensorTrig, OUTPUT); 
   pinMode(leftSensorEcho, INPUT);
-//  pinMode(rightSensorTrig, OUTPUT); 
-//  pinMode(rightSensorEcho, INPUT);
+  pinMode(rightSensorTrig, OUTPUT); 
+  pinMode(rightSensorEcho, INPUT);
 }
 
 void loop() {
-   //Right Servo Code:
-     distance = calculateDistance();
+   //Left Servo Code:
+     leftDistance = calculateDistanceLeft();
+     rightDistance = calculateDistanceRight();
      
-  if(distance < 20){
+  if(leftDistance < 20){
     servoMovement(85, 160, leftServo); 
     delay(1000);
   }
 
-//servoMovement(70, 200, rightServo);     
+  if(rightDistance < 20){
+    servoMovement(70, 200, rightServo); 
+    delay(1000);
+  }  
 }
 
-//Sensor
-int calculateDistance(){ 
-  
+//Left Sensor
+int calculateDistanceLeft(){ 
+  long duration;
   digitalWrite(leftSensorTrig, LOW); 
   delayMicroseconds(2);
   // Sets the trigPin on HIGH state for 10 micro seconds
@@ -46,9 +50,24 @@ int calculateDistance(){
   delayMicroseconds(10);
   digitalWrite(leftSensorTrig, LOW);
   duration = pulseIn(leftSensorEcho, HIGH); // Reads the echoPin, returns the sound wave travel time in microseconds
-  distance= duration*0.034/2;
-  return distance;
+  leftDistance= duration*0.034/2;
+  return leftDistance;
 }
+
+//Right Sensor
+int calculateDistanceRight(){ 
+  long duration;
+  digitalWrite(rightSensorTrig, LOW); 
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(rightSensorTrig, HIGH); 
+  delayMicroseconds(10);
+  digitalWrite(rightSensorTrig, LOW);
+  duration = pulseIn(rightSensorEcho, HIGH); // Reads the echoPin, returns the sound wave travel time in microseconds
+  rightDistance = duration*0.034/2;
+  return rightDistance;
+}
+
 
 void servoMovement(int start, int end, Servo servo){
       for(angle = start; angle < end; angle += 1)    // command to move from 0 degrees to 180 degrees 
